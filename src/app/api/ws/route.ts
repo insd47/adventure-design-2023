8678136bba2aff25bdebce9dfe6ebc72bd01e674
insd47 <me@insd.dev>
2023-12-26 10:12:27 +0900

    debug: deviceId

--- src/app/api/ws/route.ts
+++ src/app/api/ws/route.ts
@@ -16,6 +16,8 @@ export async function SOCKET(
   const url = request.url?.split("device_id=");
   const deviceId = url && url.length > 1 ? url[1].split("&")[0] : null;
 
+  console.log(deviceId, "connecs to:", process.env.DEVICE_ID);
+
   if (deviceId === process.env.DEVICE_ID) {
     arduinoClient = client;
     console.log("arduino connected!");
